7443ec138d9cba86dfce5f80591ce027bb19182e
Priyanka123-cyber <susanpriyankasebastian@gmail.com>
2024-11-26 11:04:35 +0530

    second commit

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import MainCarousel from './MainCarousel'
 import HomeSectionCarousel from './HomeSectionCarousel'
+import { mens_kurta } from '../data/menKurta'
+import Footer from './Footer'
 
 const HomePage = () => {
   return (
     <div>
         <MainCarousel/>
         <div className='py-20 space-y-10 flex flex-col justify-center px-5 lg:px-10'>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's footwear"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Women's footwear"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"New Arrivals"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Formal Collection"}/>
         </div>
+        <Footer/>
     </div>
   )
 }
