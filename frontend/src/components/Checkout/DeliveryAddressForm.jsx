1292347c1299f295ba8013afdd4bbabe9d17211d
DivyaAnilkumar <divyaanilkumar0410@gmail.com>
2024-12-19 22:05:20 +0530

    ecommerce frontend

--- frontend/src/components/Checkout/DeliveryAddressForm.jsx
+++ frontend/src/components/Checkout/DeliveryAddressForm.jsx
@@ -35,14 +35,7 @@ const DeliveryAddressForm = () => {
 
     return (
         <div>
-            <Grid container spacing={4}>
-                {/* Address Card and Delivery Button */}
-                <Grid item xs={12} lg={5} className='border rounded-e-md shadow-md h-[30.5rem] overflow-y-scroll'>
-                    <div className='p-5 py-7 border-b cursor-pointer'>
-                        {auth.user?.address.map((item, index) => <AddressCard key={index} address={item} />)}
-                        <Button sx={{ mt: 2, bgcolor: "black" }} size='large' variant='contained'>Deliver Here</Button>
-                    </div>
-                </Grid>
+            <Grid container spacing={4}>   
                 {/* Delivery Address Form */}
                 <Grid item xs={12} lg={7}>
                     <Box className="border rounded-s-md shadow-md p-5">
